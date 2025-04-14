43721db8ce99948abff90435e1fcae020847b0b6
KaykyLeandro <silva.kaykyleandro@gmail.com>
2025-04-13 22:45:08 -0300

    README updated

--- src/components/Home/About/AboutTitle.tsx
+++ src/components/Home/About/AboutTitle.tsx
@@ -1,6 +1,6 @@
 export default function AboutTitle() {
     return (
-        <h1 className="px-10 py-5 bg-black text-green-500 rounded-lg font-cutive-mono text-3xl md:text-5xl w-fit">
+        <h1 className="px-10 py-5 bg-black text-green-500 rounded-lg font-cutive-mono text-3xl md:text-5xl w-fit [text-shadow:0_0_20px_green,0_0_15px_green,0_0_5px_white]">
             Kayky Leandro
             <span className="animate-pulse">_</span>
         </h1>
